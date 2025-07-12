e0d8d5e401b82ded040f81c5efaf7a3326f0cab7
DipeshThakran <150577095+DipeshThakran@users.noreply.github.com>
2025-07-12 11:30:59 +0530

    Initial clean commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import LandingPage from './pages/landingpage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+       
+  
+        <Route path="/" element={<LandingPage />} />
+        
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
